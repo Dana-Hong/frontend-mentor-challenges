24e809217a613182d33747b2df1d9eba535db1bf
Dana <dana.hong@hotmail.com>
2023-05-29 12:25:48 -0400

    Add font families

--- Newbie/PREM-01-workit-landing-page/src/components/Hero.tsx
+++ Newbie/PREM-01-workit-landing-page/src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
         <section>
-            <h1>Data <span>tailored</span> to your needs.</h1>
+            <h1 className="font-fraunces" >Data <span>tailored</span> to your needs.</h1>
             <a href="#">Learn more</a>
             <img src="/src/assets/iPhone 11 [Front].svg" alt="iPhone11" />
         </section>
--- Newbie/PREM-01-workit-landing-page/tailwind.config.js
+++ Newbie/PREM-01-workit-landing-page/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fraunces: "'Fraunces', serif",
+        manrope: "'Manrope', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
