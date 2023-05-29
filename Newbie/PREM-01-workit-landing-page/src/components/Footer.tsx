1351e7b131c914ac123155951608233ed2f40338
Dana <dana.hong@hotmail.com>
2023-05-29 12:08:06 -0400

    Add footer

---
+++ Newbie/PREM-01-workit-landing-page/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer>
+            <img src="/src/assets/workit.svg" alt="Logo" />
+            <div>
+                <img src="/src/assets/facebook.svg" alt="facebook" />
+                <img src="/src/assets/twitter.svg" alt="twitter" />
+                <img src="/src/assets/instagram.svg" alt="instagram" />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
