3c540294c55b8bbc3e9805fafc1cf1310b565021
Dana <dana.hong@hotmail.com>
2023-05-29 11:57:04 -0400

    Add hero section

---
+++ Newbie/PREM-01-workit-landing-page/src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <section>
+            <h1>Data <span>tailored</span> to your needs.</h1>
+            <a href="#">Learn more</a>
+            <img src="/src/assets/iPhone 11 [Front].svg" alt="iPhone11" />
+        </section>
+    )
+}
