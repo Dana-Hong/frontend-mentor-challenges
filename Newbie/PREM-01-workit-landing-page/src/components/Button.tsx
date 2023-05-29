ce5492c13d436af388b00e420e2222e0e7f1c926
Dana <dana.hong@hotmail.com>
2023-05-29 18:39:05 -0400

    Add hover styles

--- Newbie/PREM-01-workit-landing-page/src/components/Button.tsx
+++ Newbie/PREM-01-workit-landing-page/src/components/Button.tsx
@@ -8,15 +8,18 @@ const Button = ({ type, children, className }: ButtonProps) => {
     return (
         <a 
             className={`
+                cursor-pointer
                 block
                 w-fit
                 py-3
                 px-6
                 font-bold
-                bg-eucaplyptus 
+                bg-eucaplyptus
+                border-2
+                border-eucaplyptus
                 text-dark-purple 
                 hover:bg-dark-purple 
-                hover:text-white hover:underline
+                hover:text-white hover:underline hover:decoration-eucaplyptus hover:decoration-[3px]
                 ${className}
                 `}>
             {children}
--- Newbie/PREM-01-workit-landing-page/src/components/Nav.tsx
+++ Newbie/PREM-01-workit-landing-page/src/components/Nav.tsx
@@ -2,7 +2,7 @@ const Nav = () => {
     return (
         <nav className="relative z-10 bg-dark-purple flex justify-between pt-8 px-4">
             <img src="/src/assets/workit-white.svg" alt="Logo" />
-            <a className="font-bold text-white underline decoration-eucaplyptus underline-offset-8 decoration-[3px]" href="">Apply for access</a>
+            <a className="font-bold text-white hover:text-eucaplyptus underline decoration-eucaplyptus underline-offset-8 decoration-[3px]" href="">Apply for access</a>
         </nav>
     )
 }
