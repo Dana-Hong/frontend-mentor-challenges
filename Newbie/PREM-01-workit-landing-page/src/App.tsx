e703bd901229be5b8fb03c61bed93728d492e24a
Dana <dana.hong@hotmail.com>
2023-05-30 17:05:08 -0400

    Add desktop styles

--- Newbie/PREM-01-workit-landing-page/src/App.tsx
+++ Newbie/PREM-01-workit-landing-page/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Nav from "./components/Nav"
 import Hero from "./components/Hero"
 import Footer from "./components/Footer"
@@ -5,49 +7,71 @@ import Footer from "./components/Footer"
 import Button from "./components/Button"
 
 function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+      console.log(screenSize);
+    } 
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+
+  }, [screenSize]);
+
   return (
     <div>
       <Nav />
       <main>
-        <Hero />
-        <section className="relative -z-10 bg-main-background min-[376px]:bg-tablet-main-background bg-bottom bg-no-repeat -translate-y-24 flex flex-col gap-10 sm:gap-12 pt-44 pb-16 px-4">
-          <div className="flex flex-col sm:flex-row sm:justify-center items-center gap-6 sm:gap-8">
+        <Hero screenSize={screenSize} />
+        <section className="relative -z-10 bg-main-background min-[376px]:bg-tablet-main-background md:bg-desktop-main-background bg-bottom bg-no-repeat -translate-y-24 flex flex-col lg:max-w-[1440px] lg:justify-evenly lg:mx-auto lg:flex-row gap-10 sm:gap-12 pt-44 pb-16 lg:pb-44 px-4">
+          <div className="flex flex-col sm:flex-row lg:flex-col sm:justify-center items-center gap-6 sm:gap-8">
             <div>
               <img src="/src/assets/num1.svg" alt="" />
             </div>
             <div className="flex flex-col gap-4 text-center sm:text-left sm:max-w-lg">
-              <p className="font-fraunces text-dark-purple text-3xl">Actionable Insights</p>
-              <p className="text-davys-gray leading-7">Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics.</p>
+              <p className="font-fraunces text-dark-purple text-3xl lg:text-center">Actionable Insights</p>
+              <p className="text-davys-gray leading-7 lg:max-w-sm">Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics.</p>
             </div>
           </div>
-          <div className="flex flex-col sm:flex-row sm:justify-center items-center gap-6 sm:gap-8">
+          <div className="flex flex-col sm:flex-row lg:flex-col sm:justify-center items-center gap-6 sm:gap-8">
             <div>
               <img src="/src/assets/num2.svg" alt="" />
             </div>
             <div className="flex flex-col gap-4 text-center sm:text-left sm:max-w-lg">
-              <p className="font-fraunces text-dark-purple text-3xl">Data-driven decisions</p>
-              <p className="text-davys-gray leading-7">Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.</p>
+              <p className="font-fraunces text-dark-purple text-3xl lg:text-center">Data-driven decisions</p>
+              <p className="text-davys-gray leading-7 lg:max-w-sm">Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.</p>
             </div>
           </div>
-          <div className="flex flex-col sm:flex-row sm:justify-center items-center gap-6 sm:gap-8">
+          <div className="flex flex-col sm:flex-row lg:flex-col sm:justify-center items-center gap-6 sm:gap-8">
             <div>
               <img src="/src/assets/num3.svg" alt="" />
             </div>
             <div className="flex flex-col gap-4 text-center sm:text-left sm:max-w-lg">
-              <p className="font-fraunces text-dark-purple text-3xl">Always affordable</p>
-              <p className="text-davys-gray leading-7">Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.</p>
+              <p className="font-fraunces text-dark-purple text-3xl lg:text-center">Always affordable</p>
+              <p className="text-davys-gray leading-7 lg:max-w-sm">Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.</p>
             </div>
           </div>
         </section>
-        <section className="mx-auto flex flex-col items-center sm:items-end sm:max-w-2xl">
-          <img className="sm:self-start" src="/src/assets/image-removebg-preview(363).png" alt="" />
-          <div className="relative mx-4 min-[376px]:max-w-lg -translate-y-14 sm:-translate-y-32 sm:translate-x-4 bg-dark-purple text-white p-8 sm:px-14 sm:pt-12 sm:pb-14 text-center sm:text-left">
+        <section className="mx-auto flex flex-col items-center sm:items-end sm:max-w-2xl lg:max-w-6xl">
+          {screenSize < 1023 
+            ? 
+            <img className="sm:self-start" src="/src/assets/image-removebg-preview(363).png" alt="founder" />
+            :
+            <img className="self-start lg:translate-x-20 lg:translate-y-32" src="/src/assets/desktop-founder-headshot.png" alt="founder" />
+
+          }
+          <div className="relative mx-4 min-[376px]:max-w-lg -translate-y-14 sm:-translate-y-32 sm:translate-x-4 bg-dark-purple text-white p-8 sm:px-14 sm:pt-12 sm:pb-14 text-center sm:text-left lg:max-w-3xl">
             <p className="font-fraunces text-[2rem] sm:text-[3rem] leading-[3rem] sm:leading-[3.5rem]">Be the first to test</p>
-            <p className="pt-4 sm:pt-6 pb-6 leading-7">Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I'll be in touch to schedule a call.</p>
+            <p className="pt-4 sm:pt-6 pb-6 leading-7 lg:max-w-xl">Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I'll be in touch to schedule a call.</p>
             <Button className="mx-auto sm:mx-0" >
               Apply for access
             </Button>
-            <img className="absolute right-0 -bottom-28" src="/src/assets/founder-bg-spiral.svg" alt="" />
+            <img className="absolute right-0 lg:right-10 -bottom-28 lg:-bottom-20" src="/src/assets/founder-bg-spiral.svg" alt="" />
           </div>
         </section>
       </main>
--- Newbie/PREM-01-workit-landing-page/src/components/Nav.tsx
+++ Newbie/PREM-01-workit-landing-page/src/components/Nav.tsx
@@ -1,6 +1,6 @@
 const Nav = () => {
     return (
-        <nav className="relative z-10 bg-dark-purple flex justify-between pt-8 px-4">
+        <nav className="relative z-10 bg-dark-purple flex justify-between pt-8 px-4 mx-auto max-w-[1440px]">
             <img src="/src/assets/workit-white.svg" alt="Logo" />
             <a className="font-bold text-white hover:text-eucaplyptus underline decoration-eucaplyptus underline-offset-8 decoration-[3px]" href="">Apply for access</a>
         </nav>
